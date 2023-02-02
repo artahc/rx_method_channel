c90105f9d66cd6e573da6de00145c32ae4306b0d
artahc <artahace@gmail.com>
2023-02-02 18:53:23 +0800

    added example method channel for macos

--- example/macos/Runner/MainFlutterWindow.swift
+++ example/macos/Runner/MainFlutterWindow.swift
@@ -15,6 +15,45 @@ class MainFlutterWindow: NSWindow {
     }
     
     private func initializeMethodChannel(_ binaryMessenger: FlutterBinaryMessenger) {
-        _ = RxMethodChannel(channelName: "test_channel", binaryMessenger: binaryMessenger)
+        let channel = RxMethodChannel(channelName: "test_channel", binaryMessenger: binaryMessenger)
+
+        channel.registerSingle("returnmyint") { (args: Argument) in
+            let value = args["myInt"] as! Int
+            
+            return Single.just(value)
+        }
+        
+        channel.registerCompletable("completable") { (args: Argument) in
+            return Completable.create { (observer : @escaping Completable.CompletableObserver) in
+                print("Print something")
+                observer(.completed)
+                return Disposables.create {}
+            }
+        }
+        
+        channel.registerObservable("observableint") { (args: Argument) in
+            let multiplier = args["multiplier"] as! Int
+            return Observable<Int>.concat([Observable.just(1), Observable.just(2), Observable.just(3)])
+                .map { $0 * multiplier }
+        }
+        
+        channel.registerObservable("periodicObservable") { (args: Argument) in
+            return Observable<Int>.interval(.seconds(2), scheduler: MainScheduler.instance).map { $0 }
+        }
+        
+        channel.registerObservable("observableerror") { (args: Argument) in
+            return Observable.concat([Observable.just(1), Observable.just(2), Observable.error(TestError.testError)])
+        }
+        
+        channel.registerObservable("throwingobservable") { (args: Argument) in
+            return Observable.create { (observer: AnyObserver) in
+                observer.onError(TestError.testError)
+                return Disposables.create{}
+            }
+        }
     }
+}
+
+enum TestError: Error {
+    case testError
 }
